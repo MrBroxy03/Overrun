b2faa4c4f88360ef4779ef3e0a9d5d5a82dfe26c
MrBroxy03 <bruno.miguel.heleno003@gmail.com>
2025-06-04 17:46:23 +0100

    Intro super fixe

--- Assets/Scripts/Scenes/TitleScreen.cs
+++ Assets/Scripts/Scenes/TitleScreen.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class TitleScreen : MonoBehaviour
 {
     public Animator animHermes;
     public Animator animEffect;
+    public Animator intro;
+    public GameObject introgmobj;
     public GameObject blackScreen;
-    
-    public float changeScene = 0f;
+    public GameObject particle;
+    public GameObject background;
+    public Canvas Canvas;
 
+    public float changeScene = 0f;
+    private float timepast = 0f;
     void Start()
     {
         if (animHermes == null)
@@ -20,14 +27,39 @@ public class TitleScreen : MonoBehaviour
             animEffect = GetComponent<Animator>();
         }
 
+        introgmobj.SetActive(true);
+        intro.Play("Intro");
+
         blackScreen.SetActive(false);
     }
 
     void Update()
     {
+        float dt = Time.deltaTime;
+
+        if (timepast > 4f && particle != null)
+        {
+            background.SetActive(true);
+            Destroy(particle);
+            Canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        }
+
+        if (timepast > 5f && introgmobj != null)
+        {
+            background.SetActive(true);
+            Debug.Log("Kaboom");
+            intro.enabled = false;
+            Destroy(introgmobj);
+            Destroy(particle);
+
+        }
+       
+        timepast += dt;
+        
+
         if (changeScene > 0)
         {
-            changeScene += Time.deltaTime;
+            changeScene += dt;
         }
 
         if (changeScene >= 2)
